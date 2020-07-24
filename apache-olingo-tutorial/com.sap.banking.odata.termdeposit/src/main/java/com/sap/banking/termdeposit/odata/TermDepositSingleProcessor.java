0554e1ef45ced0781ba3b1820ee380d86a283483
Sachin Bhosale <s.bhosale@sap.com>
2020-07-24 18:18:46 +0530

    OData Single Processor code optimized

--- apache-olingo-tutorial/com.sap.banking.odata.termdeposit/src/main/java/com/sap/banking/termdeposit/odata/TermDepositSingleProcessor.java
+++ apache-olingo-tutorial/com.sap.banking.odata.termdeposit/src/main/java/com/sap/banking/termdeposit/odata/TermDepositSingleProcessor.java
@@ -4,11 +4,7 @@ import static java.util.stream.Collectors.toList;
 import static java.util.stream.Collectors.toMap;
 
 import java.io.InputStream;
-import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
-import java.math.BigDecimal;
-import java.util.Arrays;
-import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -37,11 +33,8 @@ import com.sap.banking.termdeposit.beans.TermDeposit;
 
 public class TermDepositSingleProcessor extends ODataSingleProcessor {
 
-	private  List<Class<?>> validClasses = Arrays.asList(Boolean.class, Date.class, String.class,
-			BigDecimal.class);
-
 	private TermDepositStore dbStore = TermDepositStore.getInstance();
-	
+
 	/**
 	 * Read EntitySet = getAll()
 	 */
@@ -58,7 +51,7 @@ public class TermDepositSingleProcessor extends ODataSingleProcessor {
 		return EntityProvider.writeFeed(contentType, entitySet, props,
 				EntityProviderWriteProperties.serviceRoot(getContext().getPathInfo().getServiceRoot()).build());
 	}
-	
+
 	/**
 	 * Read Entity = get(id)
 	 */
@@ -78,8 +71,7 @@ public class TermDepositSingleProcessor extends ODataSingleProcessor {
 		return EntityProvider.writeEntry(contentType, entitySet, data,
 				EntityProviderWriteProperties.serviceRoot(getContext().getPathInfo().getServiceRoot()).build());
 	}
-	
-	
+
 	/**
 	 * Read Entity SimpleProperty = get(id)/Name
 	 * 
@@ -87,75 +79,36 @@ public class TermDepositSingleProcessor extends ODataSingleProcessor {
 	@Override
 	public ODataResponse readEntitySimpleProperty(GetSimplePropertyUriInfo uriInfo, String contentType) throws ODataException {
 
-//		// get id for Entity
-//		String id = uriInfo.getKeyPredicates().get(0).getLiteral();
-//
-//		// Get record from Backend
-//		TermDeposit result = dbStore.getTermDeposit(id);
-//
-//		// Get Properties for Entity
-//		Map<String, Object> entityProps = getPropertiesFromObject(result);
-//
-//		// initialize propValue with Parent props
-//		Object propValue = entityProps;
-//		EdmProperty finalEdmProperty = null;
-//		for (EdmProperty edmProperty : uriInfo.getPropertyPath()) {
-//			// if properties map present i.e. if ComplexProperty, set new properties as propValue
-//			if (propValue instanceof Map) {
-//				propValue = ((Map<String, Object>) propValue).get(edmProperty.getName());
-//			}
-//			finalEdmProperty = edmProperty;
-//		}
-
 		Map<EdmProperty, Object> propValMap = readEntityProperty(uriInfo.getKeyPredicates().get(0), uriInfo.getPropertyPath());
 
 		return EntityProvider.writeProperty(contentType, (EdmProperty) propValMap.keySet().toArray()[0], propValMap.values().toArray()[0]);
 	}
 
-	
 	@Override
 	public ODataResponse readEntityComplexProperty(GetComplexPropertyUriInfo uriInfo, String contentType) throws ODataException {
-//		String id = uriInfo.getKeyPredicates().get(0).getLiteral();
-//		TermDeposit result = dbStore.getTermDeposit(id);
-//		Map<String, Object> data = getPropertiesFromObject(result);
-//
-//		Object propValue = data;
-//		EdmProperty finalEdmProperty = null;
-//		for (EdmProperty edmProperty : uriInfo.getPropertyPath()) {
-//			if (propValue instanceof Map) {
-//				propValue = ((Map<String, Object>) propValue).get(edmProperty.getName());
-//			}
-//			finalEdmProperty = edmProperty;
-//		}
 
 		Map<EdmProperty, Object> propValMap = readEntityProperty(uriInfo.getKeyPredicates().get(0), uriInfo.getPropertyPath());
 
 		return EntityProvider.writeProperty(contentType, (EdmProperty) propValMap.keySet().toArray()[0], propValMap.values().toArray()[0]);
 	}
-	
-	
 
 	@Override
-	public ODataResponse readEntitySimplePropertyValue(GetSimplePropertyUriInfo uriInfo, String contentType)
-			throws ODataException {
-//		String id = uriInfo.getKeyPredicates().get(0).getLiteral();
-//		TermDeposit result = dbStore.getTermDeposit(id);
-//		Map<String,Object> data = getPropertiesFromObject(result);
-//	
-//		Object propValue = data;
-//		EdmProperty finalEdmProperty = null;
-//		for(EdmProperty edmProperty: uriInfo.getPropertyPath()) {
-//			if(propValue instanceof Map) {
-//				propValue = ((Map<String, Object>)propValue).get(edmProperty.getName());
-//			}
-//			finalEdmProperty = edmProperty;
-//		}
-		
+	public ODataResponse readEntitySimplePropertyValue(GetSimplePropertyUriInfo uriInfo, String contentType) throws ODataException {
+
 		Map<EdmProperty, Object> propValMap = readEntityProperty(uriInfo.getKeyPredicates().get(0), uriInfo.getPropertyPath());
-		
+
 		return EntityProvider.writePropertyValue((EdmProperty) propValMap.keySet().toArray()[0], propValMap.values().toArray()[0]);
 	}
-	
+
+	/**
+	 * Read Entity Property - Simple, Complex, Nested
+	 * 
+	 * @param key
+	 * @param propertyPaths
+	 * @return
+	 * @throws ODataException
+	 */
+	@SuppressWarnings("unchecked")
 	private Map<EdmProperty, Object> readEntityProperty(KeyPredicate key, List<EdmProperty> propertyPaths) throws ODataException {
 		// get id for Entity
 		String id = key.getLiteral();
@@ -183,107 +136,139 @@ public class TermDepositSingleProcessor extends ODataSingleProcessor {
 
 		return edmPropertyValueMap;
 	}
-	
+
+	/**
+	 * Create Entity = / + Post
+	 */
 	@Override
-	public ODataResponse createEntity(PostUriInfo uriInfo, InputStream content, String requestContentType,
-			String contentType) throws ODataException {
-		
+	public ODataResponse createEntity(PostUriInfo uriInfo, InputStream content, String requestContentType, String contentType) throws ODataException {
+
 		// No support for creating and linking new entry
-		if(uriInfo.getNavigationSegments().size()>0) {
+		if (uriInfo.getNavigationSegments().size() > 0) {
 			throw new ODataNotImplementedException();
 		}
-		
+
 		// No support for media resources
-		if(uriInfo.getStartEntitySet().getEntityType().hasStream()) {
+		if (uriInfo.getStartEntitySet().getEntityType().hasStream()) {
 			throw new ODataNotImplementedException();
 		}
-		
-		EntityProviderReadProperties properties = EntityProviderReadProperties.init().mergeSemantic(false).build();
-		
-		ODataEntry entry = EntityProvider.readEntry(requestContentType, uriInfo.getStartEntitySet(), content, properties);
-		//if something	 goes wrong in deserialization this is managed via the ExceptionMapper
-        //no need for an application to do exception handling here an convert the exceptions in HTTP exceptions
-		
-		Map<String, Object> requestProperties = entry.getProperties();
-		
-		Map<String, Object> result = null;
-		// create Object from
+
+		ODataEntry entry = EntityProvider.readEntry(requestContentType, uriInfo.getStartEntitySet(), content,
+				EntityProviderReadProperties.init().mergeSemantic(false).build());
+
+		Map<String, Object> requestEntityProperties = entry.getProperties();
+
+		Map<String, Object> createResult = null;
+
 		try {
 			EdmEntityType entityType = uriInfo.getStartEntitySet().getEntityType();
+
+			// Fully Qualified Name = EntityType string representation i.e. Namespace + Name
 			Class<?> entitClass = Class.forName(entityType.toString());
-			
-			Object object = entitClass.getConstructors()[0].newInstance(new Object[] {});
-			setPropertiesToObject(object, requestProperties);
-			
-			TermDeposit newTermDeposit = dbStore.addTermDeposit((TermDeposit)object);
-			
-			result = getPropertiesFromObject(newTermDeposit);
-			
+
+			// Create Entity Object
+			Object entityObject = entitClass.getConstructors()[0].newInstance(new Object[] {});
+			// set Properties from request
+			setPropertiesToObject(entityObject, requestEntityProperties);
+
+			// Persist entity - backend call - expected to set Id
+			TermDeposit dbTermDeposit = dbStore.addTermDeposit((TermDeposit) entityObject);
+
+			// get Properties for newly created Entity
+			createResult = getPropertiesFromObject(dbTermDeposit);
+
 		} catch (Exception e) {
 			e.printStackTrace();
-		} 
-		
-		return EntityProvider.writeEntry(contentType, uriInfo.getStartEntitySet(), result, EntityProviderWriteProperties.serviceRoot(getContext().getPathInfo().getServiceRoot()).build());
+		}
+
+		return EntityProvider.writeEntry(contentType, uriInfo.getStartEntitySet(), createResult,
+				EntityProviderWriteProperties.serviceRoot(getContext().getPathInfo().getServiceRoot()).build());
 	}
-	
-	private String propertyName(Method m ) {
-		return m.getName().substring(3);
-	}
-	
-	
+
 	/**
-	 * Get List of Map
+	 * Set Properties from Request to Entity Object
 	 * 
-	 * @param objects
+	 * @param source
+	 * @param props
 	 * @return
 	 */
-	private List<Map<String,Object>> getPropertiesForList(List<?> objects){
-		return objects.stream()
-			.map(this::getPropertiesFromObject)
-			.collect(toList());
-	}
-	
 	private Object setPropertiesToObject(Object source, Map<String, Object> props) {
-		
+
 		Stream.of(source.getClass().getMethods())
-			.filter(this::isSetterMethod)
-			.filter(m -> props.keySet().contains(propertyName(m)))
-			.forEach(m -> invokeSetMethod(m, source, props.get(propertyName(m))));
+				// only setter methods
+				.filter(this::isSetterMethod)
+				// only setter for which request Property present
+				.filter(m -> props.keySet().contains(propertyName(m)))
+				// invoke setter for every request property with value from request
+				.forEach(m -> invokeSetMethod(m, source, props.get(propertyName(m))));
+
 		return source;
 	}
 
-	private Object invokeSetMethod(Method m, Object obj, Object arg) {
+	/**
+	 * Get Property Name from setter method name
+	 * 
+	 * @param m
+	 * @return
+	 */
+	private String propertyName(Method m) {
+		return m.getName().substring(3);
+	}
+
+	/**
+	 * Invoke setter method on EntityObject with arg as values. This is called
+	 * recursively to fill Complex Types
+	 * 
+	 * @param m
+	 * @param obj
+	 * @param methodArgs
+	 * @return
+	 */
+	@SuppressWarnings("unchecked")
+	private Object invokeSetMethod(Method m, Object obj, Object methodArgs) {
 		try {
-			if(arg instanceof Map) {
+			// if prop value / arg is Map i.e. Property has ComplexType
+			if (methodArgs instanceof Map) {
+				// Create ComplexType object
 				Class<?> complexTypeClass = m.getParameterTypes()[0];
-				Object childObj = complexTypeClass.newInstance();
-				arg = setPropertiesToObject(childObj, (Map<String, Object>) arg);
+				Object complexTypeObj = complexTypeClass.newInstance();
+
+				// Get ComplexType Object from properties. Note - this is recursive call so it
+				// will work for nested complex types
+				methodArgs = setPropertiesToObject(complexTypeObj, (Map<String, Object>) methodArgs);
 			}
-			m.invoke(obj, arg);
-		} catch (IllegalAccessException | IllegalArgumentException | InvocationTargetException | InstantiationException e) {
+			// call setter - for COmplex type above if block gets the Object
+			m.invoke(obj, methodArgs);
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 		return obj;
 	}
-	
-	
+
+	/**
+	 * Get Properties for List of Objects
+	 * 
+	 * @param objects
+	 * @return
+	 */
+	private List<Map<String, Object>> getPropertiesForList(List<?> objects) {
+		return objects.stream().map(this::getPropertiesFromObject).collect(toList());
+	}
+
 	/**
 	 * Get Map of Property Name and Value from Object
 	 * 
 	 * @param object
 	 * @return
 	 */
-	private  Map<String, Object> getPropertiesFromObject(Object object) {
+	private Map<String, Object> getPropertiesFromObject(Object object) {
 
-		Map<String, Object> propertiesMap = Stream.of(object.getClass().getMethods())
-				.filter(this::isGetMethod)
-				.filter(m -> invokeGetMethod(m, object) != null)
+		Map<String, Object> propertiesMap = Stream.of(object.getClass().getMethods()).filter(this::isGetMethod).filter(m -> invokeGetMethod(m, object) != null)
 				.collect(toMap(this::getPropertyNameFromMethod, m -> invokeGetMethod(m, object)));
 
 		return propertiesMap;
 	}
 
-	
 	/**
 	 * Invoke methods of Entity
 	 * 
@@ -291,11 +276,11 @@ public class TermDepositSingleProcessor extends ODataSingleProcessor {
 	 * @param obj
 	 * @return
 	 */
-	private  Object invokeGetMethod(Method m, Object obj) {
+	private Object invokeGetMethod(Method m, Object obj) {
 		Object result = null;
 		try {
 			result = m.invoke(obj, new Object[] {});
-			if(result instanceof DepositRate) {
+			if (result instanceof DepositRate) {
 				result = getPropertiesFromObject(result);
 			}
 		} catch (Exception ex) {
@@ -303,40 +288,29 @@ public class TermDepositSingleProcessor extends ODataSingleProcessor {
 		return result;
 	}
 
-	
-
 	/**
-	 * Is valid supported return Type
+	 * Get Property name from method e.g. getOpenDate => OpenDate
 	 * 
 	 * @param m
 	 * @return
 	 */
-	private  boolean isValidReturnType(Method m) {
-		return validClasses.contains(m.getReturnType());
-	}
-
-	
-	/**
-	 * Get Property name from method e.g. getOpenDate => OpenDate
-	 * @param m
-	 * @return
-	 */
-	private  String getPropertyNameFromMethod(Method m) {
+	private String getPropertyNameFromMethod(Method m) {
 		return m.getName().substring(3);
 	}
 
-	
 	/**
 	 * Check if method is get or is method
+	 * 
 	 * @param m
 	 * @return
 	 */
-	private  boolean isGetMethod(Method m) {
+	private boolean isGetMethod(Method m) {
 		return !"getClass".equals(m.getName()) && m.getName().startsWith("get") || m.getName().startsWith("is");
 	}
-	
+
 	/**
 	 * Check if method is setter
+	 * 
 	 * @param m
 	 * @return
 	 */
